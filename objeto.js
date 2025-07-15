b04e7ad5e1e80635eb0588b9bcb83217c21577e1
Mateus Borges <mateusdantas73@icloud.com>
2025-07-15 13:47:57 -0300

    objetos

---
+++ objeto.js
@@ -0,0 +1,12 @@
+const listaCPFs = ['1234567891011', '1110987654321'];
+
+const infoPessoa = ['nome', 'José Silva', 'idade', 32];
+console.log(infoPessoa[1]);
+
+
+const objPessoa = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: 1234567891011,
+    turma: 'JavaScript'
+}
