4118dfba7182301f2924a828e6a032d0d4ae2905
Mateus Borges <mateusdantas73@icloud.com>
2025-07-15 14:25:34 -0300

    adicionando ao objetvo propriedades  não existentes

---
+++ alterando-objetos.js
@@ -0,0 +1,10 @@
+const estudantes = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: 1234567891011,
+    turma: 'JavaScript'
+}
+
+estudantes.telefone = '11948556369'
+console.log(estudantes.telefone);
+
