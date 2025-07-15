7bb091325d984d4b97518f7c13a70598b9f92931
Mateus Borges <mateusdantas73@icloud.com>
2025-07-15 14:05:38 -0300

    estrutura objeto

---
+++ estrutura-objeto.js
@@ -0,0 +1,12 @@
+const estudantes = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: 1234567891011,
+    turma: 'JavaScript'
+}
+
+console.log(estudantes.nome);
+console.log(`o nome de estudante é ${estudantes.nome}`);
+console.log(`os três primeiros números do CPF são ${estudantes.cpf.substring(0, 3)}`);
+
+
