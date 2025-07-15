cc35b8e52586454e6fab4e71fe65409e66479131
Mateus Borges <mateusdantas73@icloud.com>
2025-07-15 18:49:23 -0300

    array dentro do objeto

---
+++ dados-valores.js
@@ -0,0 +1,9 @@
+const estudantes = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: 1234567891011,
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['11948556369', '55116987456']
+}
+console.log(estudantes.telefones[1]);
