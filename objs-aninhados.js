b305dd621b91779341a8268d4e6bbb3008f82b63
Mateus Borges <mateusdantas73@icloud.com>
2025-07-15 23:26:11 -0300

    objetos em objetos

---
+++ objs-aninhados.js
@@ -0,0 +1,16 @@
+const estudantes = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: 1234567891011,
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['11948556369', '55116987456']
+}
+
+estudantes.endereco = {
+    rua: 'Rua Joseph Climber',
+    numero: '45',
+    complemento: 'apto 43'
+}
+
+console.log(estudantes.endereco);
